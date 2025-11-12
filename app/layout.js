0bae951370a334d23b6909e055e9734a1251dddc
Hariharan R <rhariharan1204@gmail.com>
2025-11-12 12:54:44 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Hariharan - Software Developer",
+  title: "Portfolio of Hariharan -Freelancer",
   description:
     "This is the portfolio of Hariharan. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
   icons: {
